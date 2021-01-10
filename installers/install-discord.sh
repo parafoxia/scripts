0b784889f9671e67e2d81740387c2aae8d84d0bc
parafoxia <parafoxia@carberra.xyz>
2021-01-10 18:32:07 +0000

    Improved

--- installers/install-discord.sh
+++ installers/install-discord.sh
@@ -1,6 +1,9 @@
-sudo apt-get update
+echo "Configuring packages..."
+sudo apt-get update -qq
 sudo apt-get upgrade -qq
-sudo apt-get install -qq dpkg wget
+sudo apt-get install -qq dpkg wget libgconf-2-4 libappindicator1 linc++1
+
+echo "Installing Discord..."
 cd /tmp
-wget https://discordapp.com/api/download?platform=linux&format=deb
+wget -qO discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 sudo dpkg -i discord.deb
