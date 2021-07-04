290e4480717af2c34ab994fe7b3f919630ae6384
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-07-04 20:47:07 +0100

    Add liblzma-dev install to prevent incomplete installs

--- installers/install-python.sh
+++ installers/install-python.sh
@@ -1,7 +1,6 @@
 echo "Configuring packages..."
-sudo apt-get update -qq
-sudo apt-get upgrade -qq
-sudo apt-get install -qq build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev pkg-config
+sudo apt-get update -qq && sudo apt-get upgrade -qq
+sudo apt-get install -qq build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev pkg-config liblzma-dev
 
 echo "Downloading Python $1..."
 cd /tmp
