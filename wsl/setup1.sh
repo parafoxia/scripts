1353dccb01dce910ca70c06f4a80518abe7faacd
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-09-16 20:37:29 +0100

    Patch bug

--- wsl/setup1.sh
+++ wsl/setup1.sh
@@ -1,8 +1,8 @@
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get full-upgrade -y
-sudo sed -i 's/stretch/buster/g' /etc/apt-get/sources.list
+sudo sed -i 's/stretch/buster/g' /etc/apt/sources.list
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get full-upgrade -y
-sudo sed -i 's/buster/bullseye/g' /etc/apt-get/sources.list
-sudo sed -i 's/bullseye\/updates/bullseye-security/g' /etc/apt-get/sources.list
+sudo sed -i 's/buster/bullseye/g' /etc/apt/sources.list
+sudo sed -i 's/bullseye\/updates/bullseye-security/g' /etc/apt/sources.list
 sudo apt-get update && sudo apt-get upgrade -y --without-new-pkgs && sudo apt-get full-upgrade -y
 sudo apt-get autoremove
 
